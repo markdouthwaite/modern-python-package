2c6b0f2a07e2549b0db94dc411703bcf75cf1178
Mark Douthwaite <md637@york.ac.uk>
2023-06-16 15:49:19 +0000

    Remove docs, update package

--- src/python_pkg/__init__.py
+++ src/python_pkg/__init__.py
@@ -1,8 +1,3 @@
 """A minimal python package"""
 
 __version__ = "0.1.0"
-
-
-def example_fn():
-    """A simple example function."""
-    return 0
---
+++ src/python_pkg/dummy.py
@@ -0,0 +1,5 @@
+"""Home of the `dummy_fn` function"""
+
+def dummy_fn():
+    """A simple example function."""
+    return 0
