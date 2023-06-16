c865cf7aee5dcf1b9235734e72401065980754d8
Mark Douthwaite <md637@york.ac.uk>
2023-06-16 15:49:30 +0000

    Remove docs

--- src/python_pkg/__init__.py
+++ src/python_pkg/__init__.py
@@ -1,3 +1,7 @@
 """A minimal python package"""
 
+from .dummy import dummy_fn
+
 __version__ = "0.1.0"
+
+__all__ = ["dummy_fn"]
--- src/python_pkg/dummy.py
+++ src/python_pkg/dummy.py
@@ -1,5 +1,6 @@
 """Home of the `dummy_fn` function"""
 
+
 def dummy_fn():
     """A simple example function."""
     return 0
